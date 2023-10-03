dc9a05d85500d76843f87f3c0ab03d240ee178b2
Jason Flatt <30587-oadaeh@users.noreply.drupalcode.org>
2023-10-03 09:13:44 -0400

    Issue #3390721 by oadaeh: Improve Packer class

--- src/Packer/Packer.php
+++ src/Packer/Packer.php
@@ -8,14 +8,14 @@ use Drupal\commerce_price\Price;
 use Drupal\commerce_shipping\Packer\PackerInterface;
 use Drupal\commerce_shipping\ProposedShipment;
 use Drupal\commerce_shipping\ShipmentItem;
+use Drupal\commerce_shipping_boxpacker\Packer\PackerBox;
+use Drupal\commerce_shipping_boxpacker\Packer\PackerItem;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\physical\Calculator;
 use Drupal\physical\Weight;
 use Drupal\physical\WeightUnit;
 use Drupal\profile\Entity\ProfileInterface;
-use DVDoug\BoxPacker\Test\TestBox;
-use DVDoug\BoxPacker\Test\TestItem;
 
 class Packer implements PackerInterface {
 
@@ -29,6 +29,8 @@ class Packer implements PackerInterface {
   protected $entityTypeManager;
 
   /**
+   * Packer constructor.
+   *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    */
   public function __construct(EntityTypeManagerInterface $entityTypeManager) {
@@ -42,11 +44,16 @@ class Packer implements PackerInterface {
     return TRUE;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function pack(OrderInterface $order, ProfileInterface $shipping_profile) {
     $items = [];
+    /** @var \Drupal\commerce_order\Entity\Order $order */
     $order = $this->entityTypeManager->getStorage('commerce_order')->load($order->id());
 
     foreach ($order->getItems() as $order_item) {
+      /** @var \Drupal\physical\Weight $weight */
       $weight = $this->getWeightForOrderItem($order_item);
 
       $quantity = $order_item->getQuantity();
@@ -70,13 +77,27 @@ class Packer implements PackerInterface {
     return $proposed_shipments;
   }
 
+  /**
+   * Separates the items into boxes to ship.
+   *
+   * @param array $items
+   *   The items in the order.
+   * @param \Drupal\commerce_order\Entity\Order $order
+   *   The order.
+   * @param \Drupal\profile\Entity\ProfileInterface $shipping_profile
+   *   The shipping profile being used.
+   *
+   * @return \Drupal\commerce_shipping\ProposedShipment
+   *   The boxes to ship.
+   */
   protected function buildBoxes(array $items, $order, $shipping_profile) {
     // Stash the order items by order item ID
-    /** @var OrderItemInterface[] $order_items */
+    /** @var \Drupal\commerce_order\Entity\OrderItemInterface[] $order_items */
     $order_items = [];
     foreach ($items as $item) {
       $order_items[$item['order_item']->id()] = $item['order_item'];
     }
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorage $package_type_storage */
     $package_type_storage = $this->entityTypeManager->getStorage('commerce_package_type');
     /** @var \Drupal\commerce_shipping\Entity\PackageTypeInterface[] $package_types */
     $package_types = $package_type_storage->loadByProperties();
@@ -84,18 +105,33 @@ class Packer implements PackerInterface {
 
     foreach ($package_types as $package_type) {
       $dimensions = $package_type->getDimensions();
-      $weight = $package_type->getWeight();
+      $package_weight = $package_type->getWeight();
+      $max_weight = $package_type->getThirdPartySetting('commerce_shipping_boxpacker', 'max_weight');
+      if (empty($max_weight)) {
+        $max_weight = [
+          'number' => 10000,
+          'unit' => 'g',
+        ];
+      }
+
+      // Convert the dimensions to millimeters, so there are no errors in
+      // processing.
+      $this->convertDimensionsToMillimeters($dimensions);
+      // Convert weights to grams, so there are no errors in processing.
+      $this->convertWeightToGrams($package_weight);
+      $this->convertWeightToGrams($max_weight);
+
       $boxPacker->addBox(
-        new TestBox(
+        new PackerBox(
           $package_type->id(),
           $dimensions['length'],
           $dimensions['width'],
           $dimensions['height'],
-          $weight['number'],
+          $package_weight['number'],
           $dimensions['length'],
           $dimensions['width'],
           $dimensions['height'],
-          10000  // @todo: Make box max weight configurable
+          $max_weight['number']
         )
       );
     }
@@ -112,22 +148,35 @@ class Packer implements PackerInterface {
           'unit' => 'in',
         ];
       }
-      $weight = $item['weight']->getNumber() / $item['quantity'];
+
+      $weight = [
+        'number' => $item['weight']->getNumber() / $item['quantity'],
+        'unit' => $item['weight']->getUnit(),
+      ];
+
+      // Convert the dimensions to millimeters, so there are no errors in
+      // processing.
+      $this->convertDimensionsToMillimeters($dimensions);
+      // Convert weights to grams, so there are no errors in processing.
+      $this->convertWeightToGrams($weight);
+
       $boxPacker->addItem(
-        new TestItem(
+        new PackerItem(
           $item['order_item']->id(),
           $dimensions['width'],
           $dimensions['length'],
           $dimensions['height'],
-          $weight,
+          $weight['number'],
           FALSE
         ),
         $item['quantity']
       );
     }
 
+    /** @var \DVDoug\BoxPacker\PackedBoxList $packedBoxes */
     $packedBoxes = $boxPacker->pack();
 
+    /** @var \Drupal\commerce_shipping\ProposedShipment[] $proposed_shipments */
     $proposed_shipments = [];
     foreach ($packedBoxes as $i => $box) {
       $box_items = [];
@@ -140,8 +189,9 @@ class Packer implements PackerInterface {
           'declared_value' => new Price(0, 'USD'),
         ];
       }
-      $proposed_shipment_items = [];
 
+      /** @var \Drupal\commerce_shipping\ShipmentItem[] $proposed_shipment_items */
+      $proposed_shipment_items = [];
       foreach ($box_items as $order_item_id => $box_item_details) {
         $order_item = $order_items[$order_item_id];
         $order_item_weight = $this->getWeightForOrderItem($order_item);
@@ -155,9 +205,10 @@ class Packer implements PackerInterface {
         ]);
       }
 
-      /** @var \Drupal\commerce_shipping\Entity\PackageTypeInterface $package_type */
       $package_type_machine_name = $box->getBox()->getReference();
+      /** @var \Drupal\commerce_shipping\Entity\PackageTypeInterface $package_type */
       $package_type = $package_type_storage->load($package_type_machine_name);
+      /** @var \Drupal\commerce_shipping\ProposedShipment[] $proposed_shipments */
       $proposed_shipments[] = new ProposedShipment([
         'type' => $this->getShipmentType($order),
         'order_id' => $order->id(),
@@ -171,7 +222,15 @@ class Packer implements PackerInterface {
     return $proposed_shipments;
   }
 
+  /**
+   * Gets the weight for an item in an order.
+   *
+   * @param \Drupal\commerce_order\Entity\OrderItemInterface $order_item
+   *
+   * @return \Drupal\physical\Weight
+   */
   protected function getWeightForOrderItem(OrderItemInterface $order_item) : Weight {
+    /** @var \Drupal\commerce_product\Entity\ProductVariation $purchased_entity */
     $purchased_entity = $order_item->getPurchasedEntity();
     if ($purchased_entity !== NULL && $purchased_entity->hasField('weight') && !$purchased_entity->get('weight')->isEmpty()) {
       /** @var \Drupal\physical\Weight $weight */
@@ -193,10 +252,88 @@ class Packer implements PackerInterface {
    *   The shipment type.
    */
   protected function getShipmentType(OrderInterface $order) {
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityStorage $order_type_storage */
     $order_type_storage = $this->entityTypeManager->getStorage('commerce_order_type');
     /** @var \Drupal\commerce_order\Entity\OrderTypeInterface $order_type */
     $order_type = $order_type_storage->load($order->bundle());
 
     return $order_type->getThirdPartySetting('commerce_shipping', 'shipment_type');
   }
+
+  /**
+   * Convert dimensions to millimeters.
+   *
+   * @param array $dimensions
+   *   The dimensions of the product.
+   */
+  protected function convertDimensionsToMillimeters(array &$dimensions) {
+    if (empty($dimensions['unit'])) {
+      \Drupal::logger('commerce_shipping_boxpacker')->debug('No units were specified for the dimensions, and so they were not modified.');
+      return;
+    }
+
+    // The default is to not make any changes.
+    $value_multiplier = 1;
+
+    switch ($dimensions['unit']) {
+      case 'cm':
+        $value_multiplier = 10;
+        break;
+
+      case 'm':
+        $value_multiplier = 100;
+        break;
+
+      case 'in':
+        $value_multiplier = 25.4;
+        break;
+
+      case 'ft':
+        $value_multiplier = 304.8;
+        break;
+    }
+
+    if ($value_multiplier !== 1) {
+      $dimensions['length'] = $dimensions['length'] * $value_multiplier;
+      $dimensions['width'] = $dimensions['width'] * $value_multiplier;
+      $dimensions['height'] = $dimensions['height'] * $value_multiplier;
+      $dimensions['unit'] = 'mm';
+    }
+  }
+
+  /**
+   * Converts weight to grams.
+   *
+   * @param array $weight
+   *   The weight of the product or package.
+   */
+  protected function convertWeightToGrams(&$weight) {
+    if (empty($weight['unit'])) {
+      \Drupal::logger('commerce_shipping_boxpacker')->debug('No units were specified for the weight, and so it was not modified.');
+      return;
+    }
+
+    // The default is to not make any changes.
+    $value_multiplier = 1;
+
+    switch ($weight['unit']) {
+      case 'kg':
+        $value_multiplier = 1000;
+        break;
+
+      case 'oz':
+        $value_multiplier = 28.35;
+        break;
+
+      case 'lb':
+        $value_multiplier = 453.6;
+        break;
+    }
+
+    if ($value_multiplier !== 1) {
+      $weight['number'] = $weight['number'] * $value_multiplier;
+      $weight['unit'] = 'g';
+    }
+  }
+
 }
---
+++ src/Packer/PackerItem.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Box packing (3D bin packing, knapsack problem).
+ *
+ * Copied from dvdoug/boxpacker/tests/Test/TestItem.php.
+ *
+ * @author Doug Wright
+ */
+
+declare(strict_types=1);
+
+namespace Drupal\commerce_shipping_boxpacker\Packer;
+
+use DVDoug\BoxPacker\Item;
+use JsonSerializable;
+use ReturnTypeWillChange;
+use stdClass;
+
+class PackerItem implements Item, JsonSerializable
+{
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var int
+     */
+    private $width;
+
+    /**
+     * @var int
+     */
+    private $length;
+
+    /**
+     * @var int
+     */
+    private $depth;
+
+    /**
+     * @var int
+     */
+    private $weight;
+
+    /**
+     * @var int
+     */
+    private $keepFlat;
+
+    /**
+     * Test objects that recurse.
+     *
+     * @var stdClass
+     */
+    private $a;
+
+    /**
+     * Test objects that recurse.
+     *
+     * @var stdClass
+     */
+    private $b;
+
+    /**
+     * Item constructor.
+     */
+    public function __construct(
+        string $description,
+        int $width,
+        int $length,
+        int $depth,
+        int $weight,
+        int $allowedRotation
+    ) {
+        $this->description = $description;
+        $this->width = $width;
+        $this->length = $length;
+        $this->depth = $depth;
+        $this->weight = $weight;
+        $this->keepFlat = $allowedRotation <= 2;
+
+        $this->a = new stdClass();
+        $this->b = new stdClass();
+
+        $this->a->b = $this->b;
+        $this->b->a = $this->a;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    public function getDepth(): int
+    {
+        return $this->depth;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function getKeepFlat(): bool
+    {
+        return $this->keepFlat;
+    }
+
+    #[ReturnTypeWillChange]
+    public function jsonSerialize()/* : mixed */
+    {
+        return [
+            'description' => $this->description,
+            'width' => $this->width,
+            'length' => $this->length,
+            'depth' => $this->depth,
+            'weight' => $this->weight,
+            'keepFlat' => $this->keepFlat,
+        ];
+    }
+}
